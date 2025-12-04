a1035813478ba50960c37c574be7ff2f53f1a189
Mateusz Nowak <mateusz@nakodach.pl>
2025-12-04 11:06:03 +0100

    📦 build(deps): upgrade `Spring Boot` to `4.0.0`

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-    kotlin("jvm") version "2.2.10"
-    kotlin("plugin.spring") version "2.2.10"
-    id("org.springframework.boot") version "3.5.8"
+    kotlin("jvm") version "2.2.21"
+    kotlin("plugin.spring") version "2.2.21"
+    id("org.springframework.boot") version "4.0.0"
     id("io.spring.dependency-management") version "1.1.7"
-    kotlin("plugin.jpa") version "2.2.10"
+    kotlin("plugin.jpa") version "2.2.21"
 }
 
 group = "com.dddheroes"
@@ -22,39 +22,37 @@ repositories {
 
 extra["assertkVersion"] = "0.28.1"
 extra["axonFrameworkVersion"] = "5.0.0"
-extra["springDocOpenApiVersion"] = "2.8.14"
-extra["springBootVersion"] = "3.5.8"
+extra["springDocOpenApiVersion"] = "3.0.0"
+extra["springBootVersion"] = "4.0.0"
 extra["springAiVersion"] = "1.1.0"
 
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.axonframework.extensions.spring:axon-spring-boot-starter:${property("axonFrameworkVersion")}")
+    implementation("org.springframework.boot:spring-boot-starter-webmvc")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:${property("springDocOpenApiVersion")}")
-    implementation("org.apache.logging.log4j:log4j-api-kotlin:1.5.0")
-//    implementation("org.springframework.ai:spring-ai-starter-model-anthropic")
-//    implementation("org.springframework.ai:spring-ai-starter-mcp-server-webmvc")
+
+    implementation("org.axonframework:axon-eventsourcing:${property("axonFrameworkVersion")}")
+    implementation("org.axonframework.extensions.spring:axon-spring-boot-starter:${property("axonFrameworkVersion")}")
+
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("tools.jackson.module:jackson-module-kotlin")
+
     runtimeOnly("org.postgresql:postgresql")
 
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.springframework.boot:spring-boot-testcontainers")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testImplementation("org.testcontainers:junit-jupiter")
-    testImplementation("org.testcontainers:postgresql")
-    testImplementation("org.axonframework:axon-test:${property("axonFrameworkVersion")}")
     testImplementation("com.willowtreeapps.assertk:assertk:${property("assertkVersion")}")
-
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
 
-dependencyManagement {
-    imports {
-        mavenBom("org.springframework.boot:spring-boot-dependencies:${property("springBootVersion")}")
-        mavenBom("org.springframework.ai:spring-ai-bom:${property("springAiVersion")}")
-    }
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+    testImplementation("org.axonframework:axon-test:${property("axonFrameworkVersion")}")
+    testImplementation("org.springframework.boot:spring-boot-testcontainers")
+    testImplementation("org.testcontainers:testcontainers-junit-jupiter")
+    testImplementation("org.testcontainers:testcontainers-postgresql")
+
+    implementation("org.apache.logging.log4j:log4j-api-kotlin:1.5.0")
 }
 
 kotlin {
