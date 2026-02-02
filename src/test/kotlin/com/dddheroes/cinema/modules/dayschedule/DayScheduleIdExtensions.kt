d56c87ca2cfb30de1e86bef6cb1627534b1761a3
Mateusz Nowak <mateusz@nakodach.pl>
2026-02-02 01:30:16 +0100

    DayScheduleIdExtensions.kt - track usedDates

--- src/test/kotlin/com/dddheroes/cinema/modules/dayschedule/DayScheduleIdExtensions.kt
+++ src/test/kotlin/com/dddheroes/cinema/modules/dayschedule/DayScheduleIdExtensions.kt
@@ -3,6 +3,7 @@ package com.dddheroes.cinema.modules.dayschedule
 import java.time.Instant
 import java.time.LocalDate
 import java.time.ZoneOffset
+import java.util.Collections
 import java.util.concurrent.atomic.AtomicInteger
 import kotlin.ranges.random
 
@@ -10,12 +11,18 @@ val year = (1..294276).random()
 val month = (1..12).random()
 val day = (1..28).random()
 val generated = AtomicInteger(0)
-
 fun DayScheduleId.Companion.random(): DayScheduleId = DayScheduleId.of(
     randomDate()
 )
 
 fun randomDate(hour: Int, minute: Int): Instant = randomDate().atTime(hour, minute).toInstant(ZoneOffset.UTC)
 
-fun randomDate(): LocalDate = LocalDate
-    .of(year, month, day).plusDays(generated.getAndIncrement().toLong())
+
+private val usedDates: MutableSet<LocalDate> = Collections.synchronizedSet(mutableSetOf())
+fun randomDate(): LocalDate {
+    var date: LocalDate
+    do {
+        date = LocalDate.of(year, month, day).plusDays(generated.getAndIncrement().toLong())
+    } while (!usedDates.add(date))
+    return date
+}
