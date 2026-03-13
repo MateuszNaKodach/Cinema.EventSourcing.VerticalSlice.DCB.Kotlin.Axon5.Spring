e8fcac877c3b19c5dc14bc7970ed104e95ad55df
Mateusz Nowak <mateusz@nakodach.pl>
2026-03-13 23:23:47 +0100

    ⬆️ chore(deps): upgrade Kotlin plugins to version 2.3.10 and switch Jackson from 2 to 3 dependencies in `build.gradle.kts`

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-    kotlin("jvm") version "2.3.0"
-    kotlin("plugin.spring") version "2.3.0"
+    kotlin("jvm") version "2.3.10"
+    kotlin("plugin.spring") version "2.3.10"
     id("org.springframework.boot") version "4.0.2"
     id("io.spring.dependency-management") version "1.1.7"
-    kotlin("plugin.jpa") version "2.3.0"
+    kotlin("plugin.jpa") version "2.3.10"
 }
 
 group = "com.dddheroes"
@@ -29,13 +29,8 @@ extra["springAiVersion"] = "2.0.0-M2"
 
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa") {
-//        exclude(group = "org.springframework.boot", module = "spring-boot-jackson")
-    }
-    implementation("org.springframework.boot:spring-boot-starter-webmvc") {
-//        exclude(group = "org.springframework.boot", module = "spring-boot-jackson")
-    }
-//    implementation("org.springframework.boot:spring-boot-jackson2")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-webmvc")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:${property("springDocOpenApiVersion")}")
 
     implementation("org.axonframework:axon-eventsourcing:${property("axonFrameworkVersion")}")
@@ -44,7 +39,6 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
 
     implementation("tools.jackson.module:jackson-module-kotlin") // jackson 3
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin") // jackson 2
 
     runtimeOnly("org.postgresql:postgresql")
 
